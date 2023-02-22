0b1a3c3551228176230f7abc7dd695f58c0ced44
ultimateToraMaru <60610126+ultimateToraMaru@users.noreply.github.com>
2023-02-23 08:49:16 +0900

    あらたな作品の誕生です

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    circle(width / 2, height / 2, 50);
-  }
+  createCanvas(windowWidth, windowHeight);
+
+  strokeWeight(10);   // ふちの太さ
+  stroke(100);        // ふちの色
+  fill(240);          // 塗りの色
+
+  // noFill();           // 塗りなし
+  // noStroke();         // ふちなし
+
+  circle(width / 2, height / 2, 300); // 円
+}
+
+function draw() {
+  clear();
+  // マウス座標に円を描く
+  circle(mouseX, mouseY, 50 - (mouseX-mouseY));
+}
+
+// // 円が上に上昇するアニメーション
+// let minus = 0;
+// function draw() {
+//   clear();
+//   minus++;
+//   circle(width / 2, height / 2 - minus, 50);
+// }
